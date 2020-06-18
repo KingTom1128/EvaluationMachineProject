f1b05a764512317430ed7c0cd2ea2a21b40817db
jintao19981128 <974636731@qq.com>
2020-06-18 08:40:22 +0800

    添加

--- EvaluationMachineProject/EvaluationMachineProject/FormMain.Designer.cs
+++ EvaluationMachineProject/EvaluationMachineProject/FormMain.Designer.cs
@@ -28,21 +28,183 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.cmbNumber = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textCode = new System.Windows.Forms.TextBox();
+            this.textDesc = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textTestInput = new System.Windows.Forms.TextBox();
+            this.textTestOuput = new System.Windows.Forms.TextBox();
+            this.buttonSubmit = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
-            // Main
+            // cmbNumber
+            // 
+            this.cmbNumber.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbNumber.FormattingEnabled = true;
+            this.cmbNumber.Location = new System.Drawing.Point(128, 55);
+            this.cmbNumber.Name = "cmbNumber";
+            this.cmbNumber.Size = new System.Drawing.Size(121, 23);
+            this.cmbNumber.TabIndex = 0;
+            this.cmbNumber.SelectedIndexChanged += new System.EventHandler(this.cmbNumber_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(340, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "测试输入";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(613, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "测试输出";
+            // 
+            // textCode
+            // 
+            this.textCode.Location = new System.Drawing.Point(128, 364);
+            this.textCode.Multiline = true;
+            this.textCode.Name = "textCode";
+            this.textCode.Size = new System.Drawing.Size(742, 291);
+            this.textCode.TabIndex = 3;
+            // 
+            // textDesc
+            // 
+            this.textDesc.Location = new System.Drawing.Point(128, 170);
+            this.textDesc.Multiline = true;
+            this.textDesc.Name = "textDesc";
+            this.textDesc.ReadOnly = true;
+            this.textDesc.Size = new System.Drawing.Size(742, 150);
+            this.textDesc.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(135, 335);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "编写代码处";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(135, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "题目描述";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(135, 27);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 15);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "题号";
+            // 
+            // textTestInput
+            // 
+            this.textTestInput.Location = new System.Drawing.Point(324, 55);
+            this.textTestInput.Multiline = true;
+            this.textTestInput.Name = "textTestInput";
+            this.textTestInput.ReadOnly = true;
+            this.textTestInput.Size = new System.Drawing.Size(228, 94);
+            this.textTestInput.TabIndex = 8;
+            // 
+            // textTestOuput
+            // 
+            this.textTestOuput.Location = new System.Drawing.Point(605, 55);
+            this.textTestOuput.Multiline = true;
+            this.textTestOuput.Name = "textTestOuput";
+            this.textTestOuput.ReadOnly = true;
+            this.textTestOuput.Size = new System.Drawing.Size(228, 94);
+            this.textTestOuput.TabIndex = 9;
+            // 
+            // buttonSubmit
+            // 
+            this.buttonSubmit.Location = new System.Drawing.Point(932, 317);
+            this.buttonSubmit.Name = "buttonSubmit";
+            this.buttonSubmit.Size = new System.Drawing.Size(91, 51);
+            this.buttonSubmit.TabIndex = 10;
+            this.buttonSubmit.Text = "提交";
+            this.buttonSubmit.UseVisualStyleBackColor = true;
+            this.buttonSubmit.Click += new System.EventHandler(this.buttonSubmit_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(872, 27);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 15);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "语言";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "C",
+            "C++"});
+            this.comboBox1.Location = new System.Drawing.Point(865, 55);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 11;
+            // 
+            // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Main";
+            this.ClientSize = new System.Drawing.Size(1073, 683);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.buttonSubmit);
+            this.Controls.Add(this.textTestOuput);
+            this.Controls.Add(this.textTestInput);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textDesc);
+            this.Controls.Add(this.textCode);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbNumber);
+            this.Name = "FormMain";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Main_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cmbNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textCode;
+        private System.Windows.Forms.TextBox textDesc;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textTestInput;
+        private System.Windows.Forms.TextBox textTestOuput;
+        private System.Windows.Forms.Button buttonSubmit;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- EvaluationMachineProject/EvaluationMachineProject/FormMain.cs
+++ EvaluationMachineProject/EvaluationMachineProject/FormMain.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -24,6 +25,42 @@ namespace EvaluationMachineProject
             {
                 Application.Exit();
             }
+            string connString = "Data Source = .; Initial Catalog = EvaluationMachineProject; Integrated Security = True";
+            SqlConnection conn = new SqlConnection(connString);
+            string sql = String.Format("select Id from Problem");
+            conn.Open();
+            SqlCommand comm = new SqlCommand(sql, conn);
+            SqlDataReader dr = comm.ExecuteReader();
+            while (dr.Read())
+            {
+                string id = (string)dr[0];
+                cmbNumber.Items.Add(id);
+            }
+            dr.Close();
+            conn.Close();
+        }
+
+        private void buttonSubmit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbNumber_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string connString = "Data Source = .; Initial Catalog = EvaluationMachineProject; Integrated Security = True";
+            SqlConnection conn = new SqlConnection(connString);
+            string sql = String.Format("select * from Problem where Id = '{0}'", cmbNumber.Text);
+            conn.Open();
+            SqlCommand comm = new SqlCommand(sql, conn);
+            SqlDataReader dr = comm.ExecuteReader();
+            while (dr.Read())
+            {
+                textTestInput.Text = (string)dr["TestInput"];
+                textTestOuput.Text = (string)dr["Testoutput"];
+                textDesc.Text = (string)dr["Desc"];
+            }
+            dr.Close();
+            conn.Close();
         }
     }
 }
